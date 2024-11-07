0944d2e599ceb6d18ae794ae369e061ac42abb45
Pavel <im.lucky.jr@gmail.com>
2024-11-07 09:35:28 +0500

    новое занятие

--- 7 занятие.js
+++ 7 занятие.js
@@ -6,3 +6,17 @@ function clearDiv() {
     div.innerText = '';
 }
 
+function makeRed() {
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'red';
+}
+
+function makeGreen() {
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'green';
+}
+
+function makeBlue() {
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'blue';
+}
