e931c8b9076b7641b0889adc5382d2c064fdf73a
Pavel <im.lucky.jr@gmail.com>
2025-03-03 23:41:36 +0500

    обновления

--- занятие_2025-02-26.js
+++ занятие_2025-02-26.js
@@ -1,3 +1,15 @@
+let listDOM = document.getElementById('list');
+let inputName = document.getElementById('name');
+let selectCategory = document.getElementById('category');
+let inputPrice = document.getElementById('price');
+let inputCount = document.getElementById('count');
+
+let categories = {
+    'clothes': 'Одежда',
+    'shoes': 'Обувь',
+    'tools': 'Инструменты',
+}
+
 function discount(item) {
     let field = document.getElementById('discountDom')
     
@@ -9,4 +21,30 @@ function discount(item) {
         // чтобы перебить ранее написанный стиль с !important
         field.style.setProperty('display', 'none', 'important')
     }
+}
+
+function addTovar() {
+    let discountChoose = document.querySelector('input[name=discount]:checked');
+    
+    let cardTovar = document.createElement('div');
+    cardTovar.classList.add('tovar');
+    let card = `<div class="tovar-name">${ inputName.value }</div>
+            <div class="tovar-category">${ categories[selectCategory.value] }</div>
+            <div class="tovar-specials">Особенности: </div>
+            <div class="tovar-description">Описание</div>
+            <div class="tovar-price-count">
+                <!--<div class="tovar-price">Цена: 1000 руб.</div>-->
+                <div class="tovar-price">
+                    <div>Цена: </div>
+                    <div>
+                        <div class="tovar-price-old"></div>
+                        <div>${ inputPrice.value } руб. (${ discountChoose.value })</div>
+                    </div>
+                </div>
+                <div class="count">Количество: ${ inputCount.value } шт.</div>
+            </div>
+            <div class="tovar-close">X</div>`
+    
+    cardTovar.innerHTML = card;
+    listDOM.append(cardTovar);
 }
