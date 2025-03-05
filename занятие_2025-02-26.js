163c0d5f575d6b492f3819ea634a188fb6c947b8
Pavel <im.lucky.jr@gmail.com>
2025-03-05 19:31:13 +0500

    доделали карточку

--- занятие_2025-02-26.js
+++ занятие_2025-02-26.js
@@ -3,11 +3,18 @@ let inputName = document.getElementById('name');
 let selectCategory = document.getElementById('category');
 let inputPrice = document.getElementById('price');
 let inputCount = document.getElementById('count');
+let inputDiscount = document.getElementById('discount');
+let inputDescription = document.getElementById('description');
 
 let categories = {
     'clothes': 'Одежда',
     'shoes': 'Обувь',
     'tools': 'Инструменты',
+}
+
+let specialsObj = {
+    'bu': 'Б/У',
+    'fragile': 'Хрупкое',
 }
 
 function discount(item) {
@@ -24,23 +31,48 @@ function discount(item) {
 }
 
 function addTovar() {
+    // находим активный радио-инпут, который выбран
     let discountChoose = document.querySelector('input[name=discount]:checked');
     
+    // создаем контейнер для товара
     let cardTovar = document.createElement('div');
     cardTovar.classList.add('tovar');
-    let card = `<div class="tovar-name">${ inputName.value }</div>
-            <div class="tovar-category">${ categories[selectCategory.value] }</div>
-            <div class="tovar-specials">Особенности: </div>
-            <div class="tovar-description">Описание</div>
-            <div class="tovar-price-count">
-                <!--<div class="tovar-price">Цена: 1000 руб.</div>-->
-                <div class="tovar-price">
+    
+    // создаем html наполнение для блока с ценой
+    let price = ``;
+    
+    // проверим, а есть ли в discountChoose.value значение
+    if (discountChoose.value === 'true') {
+        // inputDiscount
+        let disc = +inputPrice.value * +inputDiscount.value / 100;
+        let newPrice = +inputPrice.value - disc;
+        price = `<div class="tovar-price">
                     <div>Цена: </div>
                     <div>
-                        <div class="tovar-price-old"></div>
-                        <div>${ inputPrice.value } руб. (${ discountChoose.value })</div>
+                        <div class="tovar-price-old">${ inputPrice.value } руб.</div>
+                        <div>${ newPrice } руб.</div>
                     </div>
-                </div>
+                </div>`
+    }
+    else {
+        price = `<div class="tovar-price">Цена: ${ inputPrice.value } руб.</div>`;
+    }
+    
+    // достаем все чекбоксы особенностей и генерируем текст
+    let specialsText = '';
+    let specials = document.querySelectorAll('input[name=specials]:checked');
+    
+    for (let i = 0; i < specials.length; i++) {
+        // console.log(specials[i].value, specialsObj[specials[i].value]);
+        specialsText += ' ' + specialsObj[specials[i].value];
+    }
+    
+    let card = `<div class="tovar-name">${ inputName.value }</div>
+            <div class="tovar-category">${ categories[selectCategory.value] }</div>
+            <div class="tovar-specials">Особенности: ${ specialsText }</div>
+            <div class="tovar-description">${ inputDescription.value }</div>
+            <div class="tovar-price-count">
+                ${ price }
                 <div class="count">Количество: ${ inputCount.value } шт.</div>
             </div>
             <div class="tovar-close">X</div>`
